5a127ff99c542c0c0cdb336e93e0f8c62184cfbc
Nathan Schucher <nathan.schucher@gmail.com>
2015-06-04 20:04:59 -0300

    Got two nodes counting together.

--- src/node/main.cpp
+++ src/node/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
+#include <asio/steady_timer.hpp>
 #include <asio.hpp>
+#include <chrono>
 
 using namespace std;
 using asio::ip::udp;
@@ -10,15 +12,17 @@ class server {
 public:
     server(asio::io_service& io_service, short listen_port, short send_port)
             : io_service_(io_service),
-              socket_(io_service, udp::endpoint(udp::v4(), listen_port))
+              socket_(io_service, udp::endpoint(udp::v4(), listen_port)),
+              timer_(io_service)
     {
         if(send_port > 0) {
             udp::resolver resolver(io_service);
             udp::resolver::query query(udp::v4(), "localhost", std::to_string(send_port));
             udp::endpoint send_point = *resolver.resolve(query);
 
-            const char *test = "test";
-            socket_.async_send_to(asio::buffer(test, strlen(test)), send_point,
+            std::array<int, 1> data { 1 };
+
+            socket_.async_send_to(asio::buffer(data), send_point,
                     [this](std::error_code /*ec*/, std::size_t /*bytes_sent*/) {
                         std::cout << "Sent kickoff message." << std::endl;
                     });
@@ -29,11 +33,13 @@ public:
 private:
     void do_receive() {
         socket_.async_receive_from(
-            asio::buffer(data_, max_length), remote_endpoint_,
+            asio::buffer(data_), remote_endpoint_,
             [this](std::error_code ec, std::size_t bytes_recvd) {
                 if(!ec && bytes_recvd > 0) {
-                    std::cout.write(data_, bytes_recvd);
-                    std::cout << endl;
+                    data_[0]++;
+                    std::cout << data_[0] << std::endl;
+                    timer_.expires_from_now(std::chrono::milliseconds(500));
+                    timer_.wait();
                     do_send(bytes_recvd);
                 }
                 else {
@@ -44,17 +50,18 @@ private:
 
     void do_send(std::size_t length) {
         socket_.async_send_to(
-            asio::buffer(data_, length), remote_endpoint_,
+            asio::buffer(data_), remote_endpoint_,
             [this](std::error_code /*ec*/, std::size_t /*bytes_sent*/){
                 do_receive();
             });
     }
 
+    asio::steady_timer timer_;
     asio::io_service &io_service_;
     udp::socket socket_;
     udp::endpoint remote_endpoint_;
     enum { max_length = 1024 };
-    char data_[max_length];
+    std::array<int, 1> data_;
 };
 
 int main(int argc, char* argv[]) {
