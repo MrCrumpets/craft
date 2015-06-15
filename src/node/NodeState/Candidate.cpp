2bd544a5eaa508a3c3e352664a721dae95fc801d
Nathan Schucher <nathan.schucher@gmail.com>
2015-06-14 23:33:28 -0300

    Added some TODO notes for implementing RPCs and Follower -> Candidate conversion.

--- src/node/NodeState/Candidate.cpp
+++ src/node/NodeState/Candidate.cpp
@@ -6,6 +6,13 @@
 
 Candidate::Candidate(asio::io_service &io_service, State *s) : NodeState(io_service, s) {
     std::cout << "I'm a candidate now!" << std::endl;
+    // TODO: Increment current term
+    // TODO: Vote for self
+    // TODO: Reset election timer
+    // TODO: Send RequestVote RPC's to all other servers (need mechanism for sending messages to all servers)
+    // TODO: If votes received from majority of servers: become leader
+    // TODO: If AppendEntries RPC received from new leader: convert to follower
+    // TODO: If election timeout elapses: start new election
 }
 
 void Candidate::AppendEntries(uint64_t term, uint64_t leaderId, uint64_t prevLogIndex,
--- src/node/NodeState/Follower.cpp
+++ src/node/NodeState/Follower.cpp
@@ -14,7 +14,11 @@ Follower::Follower(asio::io_service &io_service, State *s) : NodeState(io_servic
 
 void Follower::AppendEntries(uint64_t term, uint64_t leaderId, uint64_t prevLogIndex,
                              std::vector<uint64_t> entries, uint64_t leaderCommit) {
-
+    // TODO: reply false if term < currentTerm
+    // TODO: reply false if log doesn't contain an entry at prevLogIndex
+    // TODO: if an existing entry conflicts with a new one (same index but different terms), delete the existing entry and al that follow it
+    // TODO: Append any new entries not already in the log
+    // TODO: If leaderCommit > commitIndex, set commitEnd = min(leaderCommit, index of last new entry)
     election_timer_.expires_from_now(std::chrono::milliseconds(election_timeout));
 }
 
